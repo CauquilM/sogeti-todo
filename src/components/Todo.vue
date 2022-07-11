a451c42d0f2fc73f082f973c5d7d1e8b380205a0
MathCoder <mathieufc31@gmail.com>
2022-07-11 23:35:31 +0200

    add fake data and display the todos

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,8 +1,21 @@
 <template>
-  <div></div>
+  <div>
+    <div class="todoTest" v-for="todo in todoArray" :key="todo.id">
+      {{ todo.title }}
+      <input type="checkbox" v-model="todo.done" />
+    </div>
+  </div>
 </template>
 <script>
 export default {
   name: "Todo",
+  data() {
+    return {
+      todoArray: [
+        { id: 1, title: "My first Article", done: false },
+        { id: 2, title: "My second Article", done: false },
+      ],
+    };
+  },
 };
 </script>
