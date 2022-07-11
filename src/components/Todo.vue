c30f5417a464d94c72adb5316f17bdc946855718
MathCoder <mathieufc31@gmail.com>
2022-07-12 00:58:12 +0200

    add features ( check / uncheck, move todo to the end if finished, cross if finished) of the step 2.

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
-    <div class="todoTest" v-for="todo in todoArray" :key="todo.id">
-      {{ todo.title }}
-      <input type="checkbox" v-model="todo.done" />
+    <div
+      class="todoTest"
+      v-for="todo in todoArray"
+      :key="todo.id"
+      :style="todo.done ? 'text-decoration: line-through' : ''"
+    >
+      <span>{{todo.title}}</span>
+      <input
+        type="checkbox"
+        v-model="todo.done"
+        @change="moveToTheEnd(todo.id)"
+      />
     </div>
   </div>
 </template>
@@ -14,8 +23,21 @@ export default {
       todoArray: [
         { id: 1, title: "My first Article", done: false },
         { id: 2, title: "My second Article", done: false },
+        { id: 3, title: "My third Article", done: false },
+        { id: 4, title: "My fourth Article", done: false },
+        { id: 5, title: "My fifth Article", done: false },
+        { id: 6, title: "My sixth Article", done: false },
       ],
     };
+  },
+  methods: {
+    moveToTheEnd(id) {
+      const index = this.todoArray.findIndex((object) => {
+        return object.id === id;
+      });
+      console.log(`id ${id}, index ${index} array ${this.todoArray}`);
+      this.todoArray = this.todoArray.concat(this.todoArray.splice(index, 1));
+    },
   },
 };
 </script>
